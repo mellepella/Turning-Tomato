0ab7c3a956772d059b026f1b96c3d34909d37d4e
Melvin Wahlberg <mellepella@gmail.com>
2020-12-30 15:48:00 +0100

    Add onClick effect

--- scripts/tomato.js
+++ scripts/tomato.js
@@ -6,9 +6,28 @@ class Tomato {
     this.increaseScore();
   }
 
+  static onClick() {
+    Tomato.increaseVelocity();
+
+    const tomato = {
+      clickHeight: 225,
+      clickWidth: 221,
+      height: 255,
+      width: 250,
+    };
+
+    tomatoImage.style.width = `${tomato.clickWidth}px`;
+    tomatoImage.style.height = `${tomato.clickHeight}px`;
+
+    setTimeout(() => {
+      tomatoImage.style.width = `${tomato.width}px`;
+      tomatoImage.style.height = `${tomato.height}px`;
+    }, 100);
+  }
+
   static rotate() {
     SaveFile.current.rotationDegrees += SaveFile.current.turningVelocity;
-    tomatoImage.style.transform = `rotate(${SaveFile.current.rotationDegrees}deg)`;
+    tomatoImage.style.transform = `translate(-50%, -50%) rotate(${SaveFile.current.rotationDegrees}deg)`;
   }
 
   static decreaseVelocity() {
